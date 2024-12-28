4bb5c0d4c840eff74aa6f1d40bbe2c0b002c0f5c
wutXuan <123056562+wutXuan@users.noreply.github.com>
2024-12-28 17:54:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: '/about },
+    { title: '欢迎来到我的博客', url: '/about' },
     { title: '关注微信公众号获取更多帮助', url: '/wxgzh' }
   ],
 
