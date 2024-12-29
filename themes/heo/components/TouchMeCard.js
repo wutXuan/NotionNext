bf04cce6b0e4ed08688a094291644af7ca75f67c
wutXuan <123056562+wutXuan@users.noreply.github.com>
2024-12-29 14:07:00 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -7,6 +7,7 @@ import CONFIG from '../config'
  * 交流频道
  * @returns
  */
+/*
 export default function TouchMeCard() {
   if (!JSON.parse(siteConfig('HEO_SOCIAL_CARD', null, CONFIG))) {
     return <></>
@@ -25,17 +26,12 @@ export default function TouchMeCard() {
             </h3>
             <div
             
-              //className='absolute left-0 top-0 w-full h-full'
-              //style={{
-              //  background:
-              //    'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat'
-              //}}>
+              className='absolute left-0 top-0 w-full h-full'
+              style={{
+                background:
+                  'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat'
+              }}>
               
-              <img
-                src="https://bu.dusays.com/2023/05/16/64633c4cd36a9.png"
-                alt="Card Image"
-                className="absolute left-0 top-0 w-full h-full object-cover"
-              />
             </div>
               
           </div>
@@ -46,20 +42,55 @@ export default function TouchMeCard() {
               {siteConfig('HEO_SOCIAL_CARD_TITLE_3', null, CONFIG)}
               <div
 
-              //className='absolute left-0 top-0 w-full h-full'
-              //style={{
-              //  background:
-              //    'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat'
-              //}}>
-                
-                <img
+              className='absolute left-0 top-0 w-full h-full'
+              style={{
+                background:
+                  'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat'
+              }}>
+              
+              </div>
+            </div>
+            
+          </Link>
+        }
+      />
+    </div>
+  )
+}
+*/
+export default function TouchMeCard() {
+  if (!JSON.parse(siteConfig('HEO_SOCIAL_CARD', null, CONFIG))) {
+    return <></>
+  }
+  return (
+    <div className={'relative h-28 text-white flex flex-col'}>
+      <FlipCard
+        className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-[#4f65f0] dark:bg-yellow-600 dark:border-gray-600'
+        frontContent={
+          <div className='h-full'>
+            <h2 className='font-[1000] text-3xl'>
+              {siteConfig('HEO_SOCIAL_CARD_TITLE_1', null, CONFIG)}
+            </h2>
+            <h3 className='pt-2'>
+              {siteConfig('HEO_SOCIAL_CARD_TITLE_2', null, CONFIG)}
+            </h3>
+            <img
+              src="https://bu.dusays.com/2023/05/16/64633c4cd36a9.png"
+              alt="Card Image"
+              className="absolute left-0 top-0 w-full h-full object-cover"
+            />
+          </div>
+        }
+        backContent={
+          <Link href={siteConfig('HEO_SOCIAL_CARD_URL', null, CONFIG)}>
+            <div className='font-[1000] text-xl h-full'>
+              {siteConfig('HEO_SOCIAL_CARD_TITLE_3', null, CONFIG)}
+              <img
                 src="https://bu.dusays.com/2023/05/16/64633c4cd36a9.png"
                 alt="Card Image"
                 className="absolute left-0 top-0 w-full h-full object-cover"
               />
-              </div>
             </div>
-            
           </Link>
         }
       />
